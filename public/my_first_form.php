890b1352bcaa28a28eb1e3b94563cff877ee37c9
nrs365 <nrs365@gmail.com>
2014-05-23 10:46:06 -0500

    added a second form to send an email; includes text input for recipient, sender, subject, body, and a send button

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -13,10 +13,11 @@
 		var_dump($_POST);
 	?>
 	
-	<form method="GET" action="/proces_form.php">
+	<form method="GET">
+    	<h2>User Login</h2>
     	<p>
        		<label for="username">Username</label>
-       		<input id="username" name="username" type="text" placeholder="username">
+       		<input id="username" name="username" type="text" placeholder="username" maxlength="3">
    		</p>
    		<p>
        		<label for="password">Password</label>
@@ -25,6 +26,30 @@
     	<p>
        		<button type="submit">Log in</button>
     	</p>
+    	<p>
+    		<textarea name="post_body" rows="5" cols="120"></textarea>
 	</form>
+	<form method="GET">
+		<h2>Compose an email</h2>
+		<p>
+			<label for="to">To:</lable>
+			<input id="to" name="to" type="text" placeholder="recipient" required>
+		</p>
+		<p>
+			<lable for="from">From:</lable>
+			<input id="from" name="from" type="text" placeholder="sender" required>
+		</p>
+		<p>
+			<lable for="subject">Subject:</lable>
+			<input id="subject" name="subject" type="text">
+		</p>
+		<p>
+			<lable for="body">Body</lable>
+			<textarea name="body" rows="5" cols="50"></textarea>
+		</p>
+		<p>
+			<button type="send">Send</button>
+		</p>			 
+	</form>	
 </body>
 </html>
