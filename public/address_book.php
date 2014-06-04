28d9864777a6509e7cdee38a6726085f6e33437e
nrs365 <nrs365@gmail.com>
2014-06-04 11:51:44 -0500

    made class containing open and save functions

--- public/address_book.php
+++ public/address_book.php
@@ -1,70 +1,55 @@
 <?php
-// ^Create a new file named address_book.php in your codeup.dev public folder and open it file for editing. Commit changes and push to GitHub for each step.
-
-// Much like the address book in our example, you'll be creating an address book application that stores entries in a CSV file on your computer. In the same fashion as your todo.php application, you will want to display your entries at the top of the page, and have a form below for adding new entries. Each entry should take a name, address, city, state, zip, and phone. You can use a HTML table or definition lists for displaying the addresses.
-
-// Create a function to store a new entry. A new entry should have validate 5 required fields: name, address, city, state, and zip. Display error if each is not filled out.
-
-// Use a CSV file to save to  list after each valid entry.
-
-// Open the CSV file in a spreadsheet program or text editor and verify the contents are what you expect after adding some entries.
-
-// Refactor your code to use functions where applicable.
-
-// Open the file named address_book.php for editing. Commit changes and push to GitHub for each step.
-
-// Create a function to read the file and display all entries, just like the TODO list.
-
-// Add a delete link with a query string to delete the record. When the page reloads, the record should be gone, and the save csv should no longer have the entry.
-
-// Test that HTML and javascript cannot be executed in your application from XSS or HTML in an entry.
-// Add a destructor that echos "Class Dismissed!". Place an unset() at the end of your code. When you run your program, when does the message get displayed?
-
-//Remove the destructor and unset() methods. We'll cover better use cases for this method in future units.
-
 $address_book = [];
 $entry = []; 
 $filename = './addressbook.csv';
 
-function open_file($filename) {
-	$array = [];
-	if (is_readable($filename) && filesize($filename) > 0) {
-		$filesize = filesize($filename);
-   		$read = fopen($filename, 'r');
-   		while(!feof($read)) {
-   			$row = fgetcsv($read);
-			if (!empty($row)){
-				$array[] = $row;
+class Address_data_store {
+		public $filename = '';
+	function __construct($name_of_file) {
+		$this->filename = $name_of_file;
+	}	
+	function open_file() {
+		$array = [];
+		if (is_readable($this->filename) && filesize($this->filename) > 0) {
+			$filesize = filesize($this->filename);
+	   		$read = fopen($this->filename, 'r');
+	   		while(!feof($read)) {
+	   			$row = fgetcsv($read);
+				if (!empty($row)){
+					$array[] = $row;
+				}
 			}
-		}
-   		fclose($read);
-   	}
-	return $array;
-}		
+	   		fclose($read);
+	   	}
+		return $array;
+	}		
 
-function save_csv($filename, $address_book) {
-	$handle = fopen($filename,'w');
-	foreach ($address_book as $entry) {
-		fputcsv($handle, $entry);
-	}
-	fclose($handle);
-}	
-$address_book = open_file($filename);
+	function save_csv($address_book) {
+		$handle = fopen($this->filename,'w');
+		foreach ($address_book as $entry) {
+			fputcsv($handle, $entry);
+		}
+		fclose($handle);
+	}	
+}
+$ads = new Address_data_store('./addressbook.csv');
+$address_book = $ads->open_file();
+//$address_book = open_file($filename);
 
 
 if(!empty($_POST)) {
 	if (empty($_POST['name']) || empty($_POST['address']) || empty($_POST['city']) || empty($_POST['zip'])) {
 	echo "Please enter all required fields.";
 	} else { 
-		$entry = [$_POST['name'], $_POST['address'], $_POST['city'], $_POST['state'], $_POST['zip']];
+		$entry = [$_POST['name'], $_POST['address'], $_POST['city'], $_POST['state'], $_POST['zip'], $_POST['phone']];
 		array_push($address_book, $entry);
-		save_csv($filename, $address_book);
+		$ads->save_csv($address_book);
 	}
 }
 if (isset($_GET['key'])) {
 	unset($address_book[$_GET['key']]);
 	header('Location: address_book.php');
-	save_csv($filename, $address_book);
+	$ads->save_csv($address_book);
 }
 ?>
 <? var_dump($address_book);?>
@@ -80,12 +65,13 @@ if (isset($_GET['key'])) {
 	
 <table border="1">
 <tr>
-	<th>Name: </th>
-	<th>Address: </th>
-	<th>City: </th>
-	<th>State:</th>
-	<th>Zip:</th>
-	<th>Delete:</th>
+	<th>Name</th>
+	<th>Address</th>
+	<th>City</th>
+	<th>State</th>
+	<th>Zip</th>
+	<th>Phone</th>
+	<th>Delete</th>
 </tr>
 <tr>
 	<?php foreach ($address_book as $key => $entry): ?>
@@ -121,6 +107,10 @@ if (isset($_GET['key'])) {
 			<label for="zip">ZIP: </label>
 				<input type="text" id="zip" name="zip" required>	
 		</p>
+		<p>
+			<label for="phone">Phone: </label>
+				<input type="text" id="phone" name="phone">
+		</p>		
 		<p>
 			<button type="submit" value="save_entry">Save Entry</button>
 		</p>
