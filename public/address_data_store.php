d911c964ceeaaa001a55d81a30521da2421f5d05
nrs365 <nrs365@gmail.com>
2014-06-05 08:50:39 -0500

    class address_data_store.php in a file; included in address_book.php

---
+++ public/address_data_store.php
@@ -0,0 +1,35 @@
+<?php
+class Address_data_store {
+	public $filename = '';
+
+	function __construct($name_of_file) {
+		$this->filename = $name_of_file;
+	}	
+	function open_file() {
+		$array = [];
+		if (is_readable($this->filename) && filesize($this->filename) > 0) {
+			$filesize = filesize($this->filename);
+	   		$read = fopen($this->filename, 'r');
+	   		while(!feof($read)) {
+	   			$row = fgetcsv($read);
+				if (!empty($row)){
+					$array[] = $row;
+				}
+			}
+	   		fclose($read);
+	   	}
+		return $array;
+	}		
+
+	function save_csv($address_book) {
+		$handle = fopen($this->filename,'w');
+		foreach ($address_book as $entry) {
+			fputcsv($handle, $entry);
+		}
+		fclose($handle);
+	}	
+	function __destruct() {
+		echo 'Class dismissed';
+	}
+}
+?>
