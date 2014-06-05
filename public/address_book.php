bd0be8cb18c313ebd230d067e6f4fbb90565e48e
nrs365 <nrs365@gmail.com>
2014-06-05 08:49:40 -0500

    make class Address_data_store.php a separate file

--- public/address_book.php
+++ public/address_book.php
@@ -2,44 +2,11 @@
 $address_book = [];
 $entry = []; 
 $filename = './addressbook.csv';
+include('address_data_store.php');
 
-class Address_data_store {
-	public $filename = '';
-
-	function __construct($name_of_file) {
-		$this->filename = $name_of_file;
-	}	
-	function open_file() {
-		$array = [];
-		if (is_readable($this->filename) && filesize($this->filename) > 0) {
-			$filesize = filesize($this->filename);
-	   		$read = fopen($this->filename, 'r');
-	   		while(!feof($read)) {
-	   			$row = fgetcsv($read);
-				if (!empty($row)){
-					$array[] = $row;
-				}
-			}
-	   		fclose($read);
-	   	}
-		return $array;
-	}		
-
-	function save_csv($address_book) {
-		$handle = fopen($this->filename,'w');
-		foreach ($address_book as $entry) {
-			fputcsv($handle, $entry);
-		}
-		fclose($handle);
-	}	
-	function __destruct() {
-		echo 'Class dismissed';
-	}
-}
 $ads = new Address_data_store('./addressbook.csv');
 $address_book = $ads->open_file();
 //$address_book = open_file($filename);
-
 
 if(!empty($_POST)) {
 	if (empty($_POST['name']) || empty($_POST['address']) || empty($_POST['city']) || empty($_POST['zip'])) {
