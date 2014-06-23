1aa03d253e95d1c8572a507aa0b2a5a3da469ca6
nrs365 <nrs365@gmail.com>
2014-06-23 16:44:41 -0500

    completed exercise to create a table with data using PDO and has a previous and next button to cycle through entries

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,24 +1,41 @@
 <?php
 
+function getOffset() {
+	$page = isset($_GET['page']) ? $_GET['page'] : 1;
+	return ($page - 1) * 4;
+}
+
 // Get new instance of PDO object
 $dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'nicole', 'bakagaki');
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+// $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
-$stmt = $dbc->query('SELECT name, location, date_established, area_in_acres FROM national_parks');
-$stmt = $dbc->query('SELECT * FROM national_parks');
+// $stmt = $dbc->query('SELECT name, location, date_established, area_in_acres FROM national_parks');
+// $stmt = $dbc->query('SELECT * FROM national_parks');
+
+$query = 'SELECT * FROM national_parks LIMIT 4 OFFSET ' . getOffset();
+//$park = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
+$count = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
+$parks = $dbc->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$numPages = ceil($count / 4);
+$next_page = $page + 1;
+$previous_page = $page - 1;
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<Title> National Parks</title>
+	<Title>National Parks</title>
 </head>
 <body>	
+	<div>
 	<table>	
     	<tr>
     		<th>Name</th>
@@ -28,14 +45,19 @@ $stmt = $dbc->query('SELECT * FROM national_parks');
    		</tr>
 		
 		<tr>
-		<? while ($row = $stmt->fetch()) : ?>
+		<? foreach ($parks as $park) : ?>
     		<tr>	
-    			<td><?= $row['name'] ?></td>
-    			<td><?= $row['location'] ?></td>
-    			<td><?= $row['date_established'] ?></td>
-    			<td><?= $row['area_in_acres'] ?></td>
+    			<td><?= $park['name']; ?></td>
+    			<td><?= $park['location']; ?></td>
+    			<td><?= $park['date_established']; ?></td>
+    			<td><?= $park['area_in_acres']; ?></td>
     		</tr>
-    	<? endwhile; ?>	
+    	<? endforeach; ?>	
     	</table>
+    	<ul class="pager">
+          <li class="previous_disabled"><a href="<?="?page=$previous_page"?>">Previous</a></li>
+          <li class="next"><a href="<?="?page=$next_page"?>">Next</a></li>
+        </ul>
+    </div>
 </body>
 </html>
